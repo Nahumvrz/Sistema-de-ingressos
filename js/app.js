121f6af8fc107bf4c2fc5467bd34aba6ac412ca4
Nahum Carpio <nahum20x20@gmil.com>
2026-02-22 23:18:42 -0300

    Mudanças e testes não tão significativos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 function comprar() {
     let tipo = document.getElementById("tipo-ingresso").value;
      let quantidade = document.getElementById("qtd").value;
+     let ingressoDisponivel = document.getElementById("qtd-pista").value;
+
 }
 
 comprar();
