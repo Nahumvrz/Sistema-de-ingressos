f5c30d38849a9761253d07ad79338b038c6cbd7d
Nahum Carpio <nahum20x20@gmil.com>
2026-03-04 23:33:24 -0300

    Verificação de número positivo acrescentada

--- js/app.js
+++ js/app.js
@@ -2,12 +2,16 @@ function comprar() {
     let tipo = document.getElementById("tipo-ingresso");
     let qtd = parseInt(document.getElementById("qtd").value);
 
-    if(tipo.value == 'pista') {
+    if(qtd <= 0) {
+        alert("Quantidade invalida, digite um valor maior do que 0");
+    } else {
+        if(tipo.value == 'pista') {
         comprarPista(qtd);
     } else if(tipo.value == "superior"){
             comprarSuperior(qtd);
     } else {
         comprarInferior(qtd);
+    }
     }
 }
 
